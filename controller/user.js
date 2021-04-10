0e66961175055a3cb187b3964c0cd8ce72e13436
new <santhiya@neophrontech.com>
2021-04-10 04:35:55 -0700

    make it better

--- controller/user.js
+++ controller/user.js
@@ -74,6 +74,67 @@ class UserController {
             }
         }
     }
+
+
+
+    async add(farm){
+		try{
+			let response = await userSchema.create(farm);
+			return { status: "success",   msg:"User Added successfully", result: response, message: "Added Successfully" };
+		} catch(error){
+			return {
+				status: "error",
+				error: errorHandler.parseMongoError(error)
+			};
+		}
+	}
+	
+	async fetch(){
+		try{
+			let response = await userSchema.find({});
+			let count=Object.keys(response).length;
+			return {
+				response: response,
+				count
+			};
+		} catch(error){
+			return {
+				status: "error",
+				error: errorHandler.parseMongoError(error)
+			};
+		}
+	}
+
+
+
+	async delete(id){
+		try{
+			let response = await userSchema.deleteOne({_id: id});
+			return {
+				status: "success",
+				response: response
+			};
+		} catch(error){
+			return {
+				status: "error",
+				error: errorHandler.parseMongoError(error)
+			};
+		}
+	}
+
+	async update(id, body) {
+
+        try {
+            let response = await userSchema.update({_id: id}, body);
+            return { status: "success", msg:"User Updated successfully",result: response, message: "Updated Successfully" };
+
+        } catch (error) {
+            return { status: "error", error: error };
+        }
+
+    }
+
+	
 }
 
        
