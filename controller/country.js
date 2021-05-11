8f030d0a47396b75d161c615a48991b2cb66405e
Manojkumarr130308012 <31697317+Manojkumarr130308012@users.noreply.github.com>
2021-05-10 23:05:08 -0700

    make it better

--- controller/country.js
+++ controller/country.js
@@ -32,7 +32,7 @@ class CountryController {
 		try{
 			let response = await countrySchema.find({});
 			let count=Object.keys(response).length;
-			return  response;
+			return response;
 		} catch(error){
 			return {
 				status: "error",
